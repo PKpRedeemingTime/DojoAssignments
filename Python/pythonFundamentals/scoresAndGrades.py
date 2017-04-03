b6a2437c6e692945191f703ec68a56512e51a6b4
Patrick Kilpatrick <patrick13kilpatrick@gmail.com>
2017-04-03 16:28:54 -0400

    adding scores and grades

---
+++ Python/pythonFundamentals/scoresAndGrades.py
@@ -0,0 +1,15 @@
+def scoresAndGrades():
+    def rando():
+        import random
+        return(random.randint(60,100))
+    for count in range (11):
+        x = rando()
+        if x <= 69:
+            print "Score: {}; Your grade is D".format(x)
+        elif x >= 70 and x <= 79:
+            print "Score: {}; Your grade is C".format(x)
+        elif x >= 80 and x <= 89:
+            print "Score: {}; Your grade is B".format(x)
+        else:
+            print "Score: {}; Your grade is a".format(x)
+    print "End of program. Bye!"
