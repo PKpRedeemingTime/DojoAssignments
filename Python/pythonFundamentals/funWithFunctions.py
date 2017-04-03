2db42db65952d70c611b6b7b1caf56c88a5b8657
Patrick Kilpatrick <patrick13kilpatrick@gmail.com>
2017-04-03 15:55:28 -0400

    adding fun with functions

---
+++ Python/pythonFundamentals/funWithFunctions.py
@@ -0,0 +1,6 @@
+def odd_even():
+    for count in range(1,2001):
+        if count % 2 == 0:
+            print "Number is{}. This is an even number.".format(count)
+        else:
+            print "Number is {}.This is an odd number.".format(count)
