2a42e0a83fde98f6c81d057c3734fdb1b49dcc54
Patrick Kilpatrick <patrick13kilpatrick@gmail.com>
2017-06-13 19:20:17 -0400

    adding time zone (incomplete)

--- MEAN/angular/usTimeZoneDisplay/src/app/app.component.ts
+++ MEAN/angular/usTimeZoneDisplay/src/app/app.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'US Time Zone Display';
 
+  today = new Date();
+
   oneClass: string;
   twoClass: string;
   threeClass: string;
@@ -18,24 +20,27 @@ export class AppComponent {
     this.oneClass = this.twoClass = this.threeClass = this.fourClass = this.fiveClass = "button";
   }
   oneClicked(): void {
-    console.log("one clicked");
     this.oneClass = "yellow";
-    console.log(this.oneClass);
+    this.today = new Date();
     this.twoClass = this.threeClass = this.fourClass = this.fiveClass = "button";
   }
   twoClicked(): void {
     this.twoClass = "yellow";
+    this.today = new Date();
     this.oneClass = this.threeClass = this.fourClass = this.fiveClass = "button";
   }
   threeClicked(): void {
     this.threeClass = "yellow";
+    this.today = new Date();
     this.twoClass = this.oneClass = this.fourClass = this.fiveClass = "button";
   }
   fourClicked(): void {
     this.fourClass = "yellow";
+    this.today = new Date();
     this.twoClass = this.threeClass = this.oneClass = this.fiveClass = "button";
   }
   fiveClicked(): void {
+    this.today = new Date();
     this.oneClass = this.twoClass = this.threeClass = this.fourClass = this.fiveClass = "button";
   }
 
