431595a4084372bf8a6dea26921081abfadf4c25
Patrick Kilpatrick <patrick13kilpatrick@gmail.com>
2018-06-07 11:44:05 -0400

    adding FirstJavaProgram

---
+++ java/fundamentals/firstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,10 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        String name = "Patrick";
+        int age = 44;
+        String hometown = "Cheverly, MD";
+        System.out.println("My name is "+name);
+        System.out.println("I am "+age+" years old");
+        System.out.println("My hometown is "+hometown);
+    }
+}
